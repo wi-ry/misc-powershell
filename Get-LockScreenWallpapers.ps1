358fe7e20c64e231d1a69fb0c1e3a420e00f52ca
Will Ryan <will@willryan.ca>
2017-11-20 21:25:09 -0800

    Much improved version of original script
    Got rid of unnecessary copies and added file type checking

--- Get-LockScreenWallpapers.ps1
+++ Get-LockScreenWallpapers.ps1
@@ -1,40 +1,34 @@
 $destination = "C:\Wallpapers\LockScreen"
 
-workflow Get-Wallpapers ($destination) {
+function Get-Wallpapers ($destination) {
+	$copyList = @()
     $images = Get-ChildItem "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" | Where-Object {$_.length -gt 100000}
     ForEach ($image in $images) {
-        Write-Output "Copying $($image.Name) to $destination..."
-        Copy-Item $($image.FullName) "$destination\$($image.Name).jpg"
-    }
-}
-
-function Remove-BadFiles ($files) {
-    ForEach ($file in $files) {
-        Add-Type -AssemblyName System.Drawing
-        try {
-			$img = [Drawing.Image]::FromFile($file)
+		$fileBytes = $null
+        $fileBytes = [System.IO.File]::ReadAllBytes($image.FullName)
+		if (([System.BitConverter]::ToString($fileBytes[0]) + [System.BitConverter]::ToString($fileBytes[1])) -eq 'FFD8') {
+			Add-Type -AssemblyName System.Drawing
+			try {
+				$img = [Drawing.Image]::FromFile($image.FullName)
+			}
+			catch {
+				Write-Warning "An error occured processing $(Split-Path -Leaf $image)"
+			}
+			if (($img.Width -lt 1920) -OR ($img.Height -lt 1080) -OR ($img.Height -gt $img.Width)) {
+				$img.Dispose()
+			}
+			else {
+				$img.Dispose()
+				$copyList += ($image.FullName)
+			}
 		}
-		catch {
-			Write-Warning "An error occured processing $(Split-Path -Leaf $file)"
-		}
-        if ($img.Height -gt $img.Width) {
-            $img.Dispose()
-            Start-Sleep 1
-            Remove-Item -Force $file
-            $file = Split-Path -Leaf $file
-            Write-Warning "$file is the wrong orientation to use as a wallpaper - Removing file..."
-        }
-        elseif (($img.Width -lt 1920) -OR ($img.Height -lt 1080)) {
-            $img.Dispose()
-            Start-Sleep 1
-            Remove-Item -Force $file
-            $file = Split-Path -Leaf $file
-            Write-Warning "$file is smaller than 1920x1080 - Removing file..."
-        }
-        else {
-            $img.Dispose()
-        }
     }
+	$copyList
+	ForEach	($file in $copyList) {
+		$fileName = Split-Path -Leaf $file
+		Write-Output "Copying $fileName to $destination..."
+		Copy-Item $file (Join-Path $destination "$fileName.jpg")
+	}
 }
 
 if(-not (Test-Path $destination)) { New-Item -Path $destination -ItemType Directory | Out-Null }
@@ -44,8 +38,4 @@ try {
 }
 catch {
     Write-Error "An error occurred attempting to copy images from the LockScreen cache!"
-}
-finally {
-    $files = (Get-ChildItem $destination -Filter *.jpg).FullName
-    Remove-BadFiles $files
 }
