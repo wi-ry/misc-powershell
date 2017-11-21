7def67a7c404740d3d477acee53fc7138f0788d5
Will Ryan <will@willryan.ca>
2017-11-20 21:47:07 -0800

    Tabbing tweaks and destination directory trickery!

--- Get-LockScreenWallpapers.ps1
+++ Get-LockScreenWallpapers.ps1
@@ -1,11 +1,22 @@
-$destination = "C:\Wallpapers\LockScreen"
+$destination = ""
+if ($destination -eq "") {
+	$destination = Read-Host "Please specify a destination path"
+	$script = (Join-Path $PSScriptRoot $MyInvocation.MyCommand.Name)	
+	$file = Get-Content $script
+	$file[0] = $file[0] -replace "destination = """"","destination = ""$destination"""
+	$file | Set-Content $script -Force
+}
+
+if(-not (Test-Path $destination)) {
+	New-Item -Path $destination -ItemType Directory | Out-Null
+}
 
 function Get-Wallpapers ($destination) {
 	$copyList = @()
-    $images = Get-ChildItem "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" | Where-Object {$_.length -gt 100000}
-    ForEach ($image in $images) {
+	$images = Get-ChildItem "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" | Where-Object {$_.length -gt 100000}
+	foreach ($image in $images) {
 		$fileBytes = $null
-        $fileBytes = [System.IO.File]::ReadAllBytes($image.FullName)
+		$fileBytes = [System.IO.File]::ReadAllBytes($image.FullName)
 		if (([System.BitConverter]::ToString($fileBytes[0]) + [System.BitConverter]::ToString($fileBytes[1])) -eq 'FFD8') {
 			Add-Type -AssemblyName System.Drawing
 			try {
@@ -22,20 +33,17 @@ function Get-Wallpapers ($destination) {
 				$copyList += ($image.FullName)
 			}
 		}
-    }
-	$copyList
-	ForEach	($file in $copyList) {
+	}
+	foreach	($file in $copyList) {
 		$fileName = Split-Path -Leaf $file
 		Write-Output "Copying $fileName to $destination..."
 		Copy-Item $file (Join-Path $destination "$fileName.jpg")
 	}
 }
 
-if(-not (Test-Path $destination)) { New-Item -Path $destination -ItemType Directory | Out-Null }
-
-try { 
-    Get-Wallpapers $destination
+try {
+	Get-Wallpapers $destination
 }
 catch {
-    Write-Error "An error occurred attempting to copy images from the LockScreen cache!"
+	Write-Error "An error occurred attempting to copy images from the LockScreen cache!"
 }
