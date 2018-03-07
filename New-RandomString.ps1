a104077ed7d2a5dda51af5b48592ff7f625f7cd6
Will Ryan <github@willryan.ca>
2018-03-07 11:13:09 -0800

    Update New-RandomString.ps1

--- New-RandomString.ps1
+++ New-RandomString.ps1
@@ -1,4 +1,9 @@
 # This could be expanded a lot, but it's a start
-function New-RandomString ([int]$length = 20) {
-	-join ((65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
+function New-RandomString ([int]$length = 20, [int]$count = $null) {
+	if($count -eq $null) { $count = 1}
+	$i = 0
+	while ($i -le $count) {
+		-join ((65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
+		$i++
+	}
 }
