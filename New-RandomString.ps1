53b0e62f73af91eda831348853eeefb96fee3871
Will Ryan <github@willryan.ca>
2018-12-11 14:22:18 -0800

    Fix number inclusion

--- New-RandomString.ps1
+++ New-RandomString.ps1
@@ -1,19 +1,16 @@
 # This could be expanded a lot, but it's a start
-function New-RandomString ([int]$length = 20, [int]$count = $null) {
-	if($count -eq $null) { $count = 1}
-	$i = 0
-	while ($i -le $count) {
-		-join ((65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
-		$i++
-	}
-}
+# Use '-incInt' to include numbers in strings generated
 
-# Now with numbers!
-function New-RandomString2 ([int]$length = 20, [int]$count = $null) {
+function New-RandomString ([int]$length = 20, [int]$count = $null, [switch]$incInt) {
 	if($count -eq $null) { $count = 1}
 	$i = 0
 	while ($i -le $count) {
-		-join ((65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
+        if ($incInt) {
+            -join ((48..57) + (65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
+        }
+        else {
+            -join ((65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
+        }
 		$i++
 	}
 }
